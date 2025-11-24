48adb70ac061346d18cdbdff57b0b44b0649b13e
420646826 <420646826@qq.com>
2025-11-24 03:13:20 +0000

    Update ffi.h

--- src/ffi.h
+++ src/ffi.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <uv.h>
 #include <napi.h>
 
@@ -29,14 +30,20 @@ namespace FFI {
 
 using namespace Napi;
 
+// ---------------- Node 20 兼容类型 ----------------
+#if NODE_MAJOR_VERSION >= 20
+using napi_finalize_t = node_api_basic_finalize;
+#else
+using napi_finalize_t = napi_finalize;
+#endif
+
 class InstanceData;
 
 /*
  * Class used to store stuff during async ffi_call() invokations.
  */
-
 class AsyncCallParams {
-  public:
+public:
     explicit AsyncCallParams(Env env_) : env(env_) {}
     Env env;
     ffi_status result;
@@ -50,11 +57,11 @@ class AsyncCallParams {
 };
 
 class FFI {
-  public:
+public:
     static Object InitializeStaticFunctions(Env env);
     static void InitializeBindings(Env env, Object target);
 
-  protected:
+protected:
     static Value FFIPrepCif(const Napi::CallbackInfo& args);
     static Value FFIPrepCifVar(const Napi::CallbackInfo& args);
     static void FFICall(const Napi::CallbackInfo& args);
@@ -69,29 +76,26 @@ class FFI {
  * pointer to proxy back to JS-land properly. It gets created by
  * `ffi_closure_alloc()`, and free'd in the closure_pointer_cb function.
  */
-
 struct callback_info {
-  callback_info() {
-  }
+    callback_info() {}
 
-  ffi_closure closure;           // the actual `ffi_closure` instance get inlined
-  void* code;                    // the executable function pointer
-  FunctionReference errorFunction;    // JS callback function for reporting caught exceptions for the process' event loop
-  FunctionReference function;         // JS callback function the closure represents
-  // these two are required for creating proper sized WrapPointer buffer instances
-  int argc;                      // the number of arguments this function expects
-  size_t resultSize;             // the size of the result pointer
-  InstanceData* instance_data;
+    ffi_closure closure;           // the actual `ffi_closure` instance get inlined
+    void* code;                    // the executable function pointer
+    FunctionReference errorFunction;    // JS callback function for reporting exceptions
+    FunctionReference function;         // JS callback function the closure represents
+    int argc;                      // number of arguments
+    size_t resultSize;             // result pointer size
+    InstanceData* instance_data;
 };
 
 class ThreadedCallbackInvokation;
 
 class CallbackInfo {
-  public:
+public:
     static Function Initialize(Env env);
     static void WatcherCallback(uv_async_t* w);
 
-  protected:
+protected:
     static void DispatchToV8(callback_info* self, void* retval, void** parameters, bool dispatched = false);
     static void Invoke(ffi_cif* cif, void* retval, void** parameters, void* user_data);
     static Value Callback(const Napi::CallbackInfo& info);
@@ -102,12 +106,9 @@ class CallbackInfo {
  *   -> WaitForExecution()     invoked
  *   -> SignalDoneExecuting()  returned
  *   -> WaitForExecution()     returned
- *
- *   ^WaitForExecution() must always be called from the thread which owns the object
  */
-
 class ThreadedCallbackInvokation {
-  public:
+public:
     ThreadedCallbackInvokation(callback_info* cbinfo, void* retval, void** parameters);
     ~ThreadedCallbackInvokation();
 
@@ -118,43 +119,44 @@ class ThreadedCallbackInvokation {
     void** m_parameters;
     callback_info* m_cbinfo;
 
-  private:
+private:
     uv_cond_t m_cond;
     uv_mutex_t m_mutex;
 };
 
 class InstanceData final {
- public:
-  explicit InstanceData(Env env_) : env(env_) {}
+public:
+    explicit InstanceData(Env env_) : env(env_) {}
 
-  Env env;
-  RefNapi::Instance* ref_napi_instance = nullptr;
+    Env env;
+    RefNapi::Instance* ref_napi_instance = nullptr;
 
-  void Dispose();
+    void Dispose();
 
 #ifdef WIN32
-  DWORD thread;
+    DWORD thread;
 #else
-  uv_thread_t thread;
+    uv_thread_t thread;
 #endif
-  uv_mutex_t mutex;
-  std::queue<ThreadedCallbackInvokation*> queue;
-  uv_async_t async;
+    uv_mutex_t mutex;
+    std::queue<ThreadedCallbackInvokation*> queue;
+    uv_async_t async;
 
-  static InstanceData* Get(Env env);
+    static InstanceData* Get(Env env);
 };
 
+// ---------------- Helper functions ----------------
 TypedArray WrapPointerImpl(Env env, char* ptr, size_t length);
 char* GetBufferDataImpl(Value val);
 
 template <typename T>
 inline TypedArray WrapPointer(Env env, T* ptr, size_t length = 0) {
-  return WrapPointerImpl(env, reinterpret_cast<char*>(ptr), length);
+    return WrapPointerImpl(env, reinterpret_cast<char*>(ptr), length);
 }
 
 template <typename T>
 inline T* GetBufferData(Value val) {
-  return reinterpret_cast<T*>(GetBufferDataImpl(val));
+    return reinterpret_cast<T*>(GetBufferDataImpl(val));
 }
 
-}
+} // namespace FFI
