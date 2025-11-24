ebefc64c80b63edb7fcf555f740838e5b22ae2e1
420646826 <420646826@qq.com>
2025-11-24 03:16:03 +0000

    Update threaded_callback_invokation.cc

--- src/threaded_callback_invokation.cc
+++ src/threaded_callback_invokation.cc
@@ -2,17 +2,15 @@
 
 namespace FFI {
 
-ThreadedCallbackInvokation::ThreadedCallbackInvokation(callback_info* cbinfo, void* retval, void** parameters) {
-  m_cbinfo = cbinfo;
-  m_retval = retval;
-  m_parameters = parameters;
-
+ThreadedCallbackInvokation::ThreadedCallbackInvokation(callback_info* cbinfo, void* retval, void** parameters)
+    : m_cbinfo(cbinfo), m_retval(retval), m_parameters(parameters) {
   uv_mutex_init(&m_mutex);
-  uv_mutex_lock(&m_mutex);
   uv_cond_init(&m_cond);
+  uv_mutex_lock(&m_mutex);  // 锁在构造时加，WaitForExecution 会释放
 }
 
 ThreadedCallbackInvokation::~ThreadedCallbackInvokation() {
+  // 确保锁已释放再销毁
   uv_mutex_unlock(&m_mutex);
   uv_cond_destroy(&m_cond);
   uv_mutex_destroy(&m_mutex);
@@ -26,6 +24,7 @@ void ThreadedCallbackInvokation::SignalDoneExecuting() {
 
 void ThreadedCallbackInvokation::WaitForExecution() {
   uv_cond_wait(&m_cond, &m_mutex);
+  uv_mutex_unlock(&m_mutex);  // 确保 Wait 后释放锁
 }
 
-}
+}  // namespace FFI
